d03d49ea7e6f4d68954e6b03c80b9c4b70830c69
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-28 15:35:42 +0900

    04. Button & inline handler

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
-export default class App extends React.Component {
-
-  render(){
-
-    return (  // handler는 항상 함수로 처리함
-      <View style={styles.container}>
-        <Text style={[styles.textBig, styles.textRed]}>react native 연습!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+export default function App() {
+  return (  // handler는 항상 함수로 처리함
+    <View style={styles.container}>
+      <Button title={'click me!'} onPress={() => console.log('Clicked')} />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 // StyleSheet는 css와 유사함. 자바스크립트 컨트롤
@@ -23,11 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textBig: {
-    fontSize: 32,
-  },
-  textRed: {
-    color: 'red',
-  },
-
 });
