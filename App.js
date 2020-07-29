f1d351f7d8c1911cb413cda5d88242705e030512
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-29 11:20:40 +0900

    18. overflowed contents

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -9,14 +9,43 @@ export default class App extends React.Component {
   render() {  
     return (
       <SafeAreaView style={styles.fullscreen}>
-        <View style={styles.container}>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
-          <StatusBar style="auto" />
-        </View>
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        <StatusBar style="auto" />
       </SafeAreaView>
     );
   }
@@ -26,13 +55,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   fullscreen: {
     flex: 1,
-  },
-  container: {
-    flex: 1,
     backgroundColor: '#fff',
     borderColor: 'red',
     borderWidth: 2,
-    justifyContent: 'center',
     alignItems: 'center',
   },
 });
