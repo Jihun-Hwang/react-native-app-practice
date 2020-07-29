8ee85645404421745daa65d31c3658cb44da5b29
Jihun-Hwang <goharrm@ajou.ac.kr>
2020-07-29 11:01:17 +0900

    17. SafeAreaView

--- App.js
+++ App.js
@@ -2,23 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+// aftera
 export default class App extends React.Component {
   render() {  
     return (
-      <View style={styles.container}>
-        <Image source={{
-            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-            width: 128,
-            height: 128,
-        }} />
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.fullscreen}>
+        <View style={styles.container}>
+          <Image source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+              width: 128,
+              height: 128,
+            }} />
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 // StyleSheet는 css와 유사함. 자바스크립트 컨트롤
 const styles = StyleSheet.create({
+  fullscreen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
